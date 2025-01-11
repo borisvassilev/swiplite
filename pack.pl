df3ae03b5ef026652509d4fe6e8ec679b9a090aa
Boris <boris.vassilev@gmail.com>
2025-01-11 17:15:03 +0200

    Bump version

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 pack_version(2).
 name(swiplite).
-version('0.2').
+version('0.3').
 title('Prolog bindings for SQLite3').
 keywords([sql, library]).
 author('Boris Vassilev', 'boris.vassilev@gmail.com').
